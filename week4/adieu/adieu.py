b8de49250a1bf6b3497d8fa6ba3202b5959243cd
zjiawwei <zjiawwei@gmail.com>
2026-02-24 22:12:04 +0800

    week4的作业补充

---
+++ week4/adieu/adieu.py
@@ -0,0 +1,12 @@
+import inflect
+
+p = inflect.engine()
+name_list = []
+while True:
+    try:
+        name = input("Name:")
+        name_list.append(name)
+    except EOFError:
+        print()
+        break
+print(f"Adieu,adieu,to {p.join(name_list)}")
